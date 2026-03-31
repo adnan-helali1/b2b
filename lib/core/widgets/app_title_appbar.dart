28cd0130ae8cba8394bbd2e6271b386f3cd33119
adnan-helali1 <helaliadnan877@gmail.com>
2026-03-31 19:05:11 +0300

    fix bugs

--- lib/core/widgets/app_title_appbar.dart
+++ lib/core/widgets/app_title_appbar.dart
@@ -28,14 +28,8 @@ class AppTitleAppbar extends StatelessWidget {
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'متجر الأسرة',
-              style: TextStyles.font18BlackBold(context),
-            ),
-            Text(
-              'سوبر ماركت',
-              style: TextStyles.font12Grey(context),
-            ),
+            Text('متجر الأسرة', style: TextStyles.font18BlackBold),
+            Text('سوبر ماركت', style: TextStyles.font12Grey),
           ],
         ),
       ],
