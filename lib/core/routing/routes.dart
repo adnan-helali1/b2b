8c7fc2fab28210011fd8960b21cba28ae2e75393
adnan-helali1 <helaliadnan877@gmail.com>
2026-04-01 17:00:15 +0300

    upgrade ui

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,5 +1,5 @@
 class Routes {
-  static const String superhomeScreen = '/superhomescreen';
-  static const String supplierHomeScreen =
-      '/supplierhomescreen';
+  static const String superHomeScreen = '/superhomescreen';
+  static const String supplierHomeScreen = '/supplierhomescreen';
+  static const String loginScreen = '/loginscreen';
 }
---
+++ lib/modules/auth/ui/widgets/app_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppTextField extends StatelessWidget {
+  final String label;
+  final String hint;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+
+  const AppTextField({
+    super.key,
+    required this.label,
+    required this.hint,
+    required this.controller,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+  }
+}
