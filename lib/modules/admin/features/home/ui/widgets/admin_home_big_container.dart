4ff31809983604788a10be5567b3e504a862ed3f
joudybakr <joodbakr2004@gmail.com>
2026-04-06 23:03:56 +0300

    supplement1_admin_home_ui_screen

---
+++ lib/modules/admin/features/home/ui/widgets/admin_home_big_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:b2b/core/theme/app_colors.dart';
+import 'package:b2b/core/theme/textstyles.dart';
+import 'package:flutter/widgets.dart';
+import 'package:b2b/core/theme/colors.dart';
+
+class BigContainer extends StatelessWidget {
+  final List<Widget> children;
+  const BigContainer({super.key, required this.children});
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+    margin: EdgeInsets.all(12),
+    width: double.infinity,
+    height: 400,
+    decoration:BoxDecoration(
+      color: AppColors.lightSurface,
+       borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: AppColors.neutral60.withOpacity(0.5),
+          width: 2,
+        ),
+    ),
+    child: Column(
+     
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: children,
+    ),
+   );
+  }
+}
